a20854f6f3cafa8776fb9947966d8d1d10871dc1
eduardaaragao <eduardaaragao18@gmail.com>
2021-07-01 17:56:31 +0100

    inserção link download

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import '../../CSS/style.css'
 import Button from '../Button'
-import {Link} from 'react-router-dom'
 
 export default class Hero extends Component {
     render(){
@@ -13,7 +12,7 @@ export default class Hero extends Component {
                         <h1>Organize o fluxo de pessoas da sua empresa.</h1>
                         <p>Um software capaz de monitorar em tempo real. CrowdZero é uma aplicação
                             que te permite ter acesso à quantidade de pessoas presentes em um espaço.</p>
-                        <Link to={"/registar"}><Button name="Requisitar acesso"/></Link>
+                        <a target='__blank' href='https://drive.google.com/uc?export=download&id=1sV_6n-ryJbCEweOs4FfCuCqYIK4V7X_g'><Button name="FAZER DONWLOAD DA APP"/></a>
                     </div>
                 </div>
             </section>
