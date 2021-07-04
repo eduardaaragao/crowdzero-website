5bfd4e6064eae145049117ee426bf537b4ee8be9
Eduarda Aragão <eduardaaragao18@gmail.com>
2021-07-04 21:21:42 +0100

    alteracao

--- src/Components/CriarLocalCard/card.js
+++ src/Components/CriarLocalCard/card.js
@@ -13,6 +13,44 @@ export default class Card extends Component{
        this.props.updated()
     }
 
+    getStatus = async () => {
+        let status = this.props.data.status
+
+        if (status === 0){
+            return "Sem Ocupação"
+        }else if (status === 1){
+            return "Baixa"
+        }else if (status === 2){
+            return "Média"
+        }else if (status === 3){
+            return "Alta"
+        }else if (status ===4){
+            return "Em Desinfeção"
+        }
+    }
+
+    getOcupacao = async () => {
+        let alto = this.props.data.qtde_reporte_alto 
+        let medio = this.props.data.qtd_reporte_baixo 
+        let baixo = this.props.data.qtd_reporte_medio
+        let maior = alto
+
+        if (medio > maior){
+            maior = medio
+        }else if (baixo > maior){
+            maior = baixo
+        }
+
+        return maior
+    }
+
+    getPorcentagem = async() => {
+        let soma = this.props.data.qtde_reporte_alto + this.props.data.qtd_reporte_baixo + this.props.data.qtd_reporte_medio
+        let maior = this.getOcupacao()
+
+        return (maior / soma) * 100
+    }
+
     render(){
         return (
             <div className="card-wrapper">
@@ -20,9 +58,9 @@ export default class Card extends Component{
                 <p style={{color: '#2C6975'}}>{this.props.data.descricao}.</p>
                 <div className="df sb">
                     <p>Ocupação</p>
-                    <p>{this.props.data.status}</p>
+                    <p>{this.getOcupacao}</p>
                 </div>
-                <Linha estado={this.props.data.status}/>
+                <Linha estado={this.getStatus}/>
                 
                 <div className="df"><p style={{color: '#205072', fontWeight: 700}}>Status: </p><p>{this.props.data.status}</p></div>
                 <div className="df sb">
