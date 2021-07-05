834906711790534ed098e2fa2c15f0e0f6cbdf40
eduardaaragao <eduardaaragao18@gmail.com>
2021-07-05 20:50:02 +0100

    Alteração link download

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -12,7 +12,7 @@ export default class Hero extends Component {
                         <h1>Organize o fluxo de pessoas da sua empresa.</h1>
                         <p>Um software capaz de monitorar em tempo real. CrowdZero é uma aplicação
                             que te permite ter acesso à quantidade de pessoas presentes em um espaço.</p>
-                        <a target='__blank' href='https://drive.google.com/uc?export=download&id=1sV_6n-ryJbCEweOs4FfCuCqYIK4V7X_g'><Button name="FAZER DOWNLOAD DA APP"/></a>
+                        <a target='__blank' href='https://drive.google.com/uc?export=download&id=1PBRL4vwodZZ4QtBPcrPkRH2Kg6D7_UWR'><Button name="FAZER DOWNLOAD DA APP"/></a>
                     </div>
                 </div>
             </section>
